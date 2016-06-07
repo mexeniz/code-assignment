d67890120a15e38be6620124f91f9d791f5ecc36
Bankde <bankde@macmini5.179.168.192.in-addr.arpa>
2016-06-07 16:24:53 +0700

    Initial Commit

---
+++ mySpreadSheet/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mySpreadSheet
+//
+//  Created by Bankde on 7/6/16.
+//  Copyright © 2016 Bankde. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
