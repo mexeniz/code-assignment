dde1a63b8690a6aa764c7b6c800e95df3e99e6d1
pawissakan-throughwave <munix@kittyhawk.wifi.bkk.throughwave.com>
2016-06-10 16:48:48 +0700

    finish echo function

--- echo/echo/main.c
+++ echo/echo/main.c
@@ -7,15 +7,35 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-//typedef int (*echo)(char* str));
+typedef void (*myEcho)(char * str);
 
-int echo(char *s);
+void echo1(char *str){
+    printf("echo1:%s\n",str);
+}
+void echo2(char *str){
+    printf("echo2:%s\n",str);
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    int a = echo();
+    
+    myEcho echo;
+    
+    //Test Echo 1
+    printf("Test Echo 1: ");
+    echo = &echo1 ;
+    char* str = (char*) malloc(sizeof(char)*50);
+    scanf("%s",str);
+    echo(str);
+    
+    //Test Echo 2
+    printf("Test Echo 2: ");
+    echo = &echo2 ;
+    scanf("%s",str);
+    echo(str);
+    
     
     return 0;
 }
